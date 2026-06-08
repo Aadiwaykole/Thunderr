91327716e90b4131a598cc69a4e40be05350f04e
aditya waykole <adityawaykole494@gmail.com>
2026-06-08 21:54:04 +0530

    lecture 4

---
+++ javascript/lecture_04/index.js
@@ -0,0 +1,50 @@
+//bitwise operator
+
+//blockchain : finance, zomato, swiggy, paymnet gateway
+
+
+let first = 120.01;
+let second  = 139.94;
+console.log(first +second )
+
+
+//parseint 
+
+console.log(Number("100x")) //its not going to convert
+
+console.log(parseInt("100x"))
+
+console.log(parseFloat("100.57ps"));
+
+
+
+//conditions
+
+// let age = 39;
+
+// if (age>20){
+//     console.log("can drive the car")
+// }
+// else{
+//     console.log("you cant")
+// }
+
+
+
+//numbers
+//new object : Math 
+
+//OTP  generate kr sakta hia ..
+
+let a = 23; 
+
+a = a*-1;
+console.log(a)
+
+console.log(Math.random())
+
+
+console.log(Math.floor(Math.random()*10+3))
+
+
+console.log(Math.floor(Math.random()*(50-45+1)+45))
