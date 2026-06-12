b15e0ff0294cb0a13513f8027e7e96003f0cdbf6
aditya waykole <adityawaykole494@gmail.com>
2026-06-12 20:52:43 +0530

    string

---
+++ javascript/lecture_04/string.js
@@ -0,0 +1,11 @@
+let str1 =  "Rohit negi"
+let str2 = 'aditya waykole '
+
+let age = 39; 
+let str3  = `aditya is ${age } years old `
+
+console.log(str3);
+
+console.log(str3.length);
+
+for(i = 0; i<)
