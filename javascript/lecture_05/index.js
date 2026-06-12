f382bc55f64edf8753e305d064be26806102180a
aditya waykole <adityawaykole494@gmail.com>
2026-06-12 21:40:01 +0530

    changes

---
+++ javascript/lecture_05/index.js
@@ -0,0 +1,49 @@
+//this lecture based on arrays only
+
+//array  = what is an array in javascript
+// 5 students = 30,40,56,78,65
+
+let marks1 = 30;
+
+let marks2 = 40;
+let marks3 = 65;
+let marks4= 78;
+let marks5 = 65;
+
+//5 variable create : 100 students ke marks
+
+// let mark = [54,3,6,4,2,54];
+// console.log(mark);
+
+// //find the number of emelent in the array
+// console.log(mark.length)
+
+//what if i have to find the individual element from the array
+
+// arr = [4,5,4,5,6]
+
+// console.log(arr[1])
+
+// for(let i= 0; i<=arr.length; i ++){  //more that count 
+//     console.log(arr[i])
+// }
+
+// arr 2  = [true, ]
+
+
+//can i update the value inside the array
+
+user = ["abc", "adity", true, 43];
+user[0]="shra"
+
+console.log(user)
+
+
+
+//push(insert element in teh array at the end )
+//pop (delete element from the end of array 
+
+let num = [43,5,3,2,54]
+num.push(48)
+console.log(num );
+pum.p
