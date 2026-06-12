2c619325c416aa8485ab2379915591ecbf55f35c
aditya waykole <adityawaykole494@gmail.com>
2026-06-12 22:48:33 +0530

    update slice and splice

--- javascript/lecture_05/index.js
+++ javascript/lecture_05/index.js
@@ -46,4 +46,47 @@ console.log(user)
 let num = [43,5,3,2,54]
 num.push(48)
 console.log(num );
-pum.p
+num.pop();
+console.log(num)
+
+//add and delete element at the start of an array
+num.unshift(40,50);
+console.log(num )
+num.shift();
+console.log(num)
+
+
+//for of loop ka use bhut karte hia 
+let nums =[5,6,4,3,9]
+for(let x of nums){
+    console.log(x )
+};
+
+
+
+//slice and splice
+// let count = [43,2,98,43,23,98];
+// const a = count.slice(1,4)  //it gives you the value from 1 to 3
+//2 to not 4 only 3 
+// console.log(a)
+ 
+// console.log(count);//there is no changes in main arrray
+
+
+//splice 
+
+let count = [43,2,98,43,23,98];
+const a  = count.splice (1,3); //second index. total 3 element from  1
+console.log(count);//changes in main array
+//splice return a new array from perticular index, 
+//second :total elemnet 
+
+
+//starting element : splice:Delete, insert karane ke liye kr sakte hia 
+//if have to to delete 30 from the array
+arr4 = [12,3,30,56,7,5]
+arr4.splice(2,1);
+console.log(arr4) 
+
+// arr = arr2 it follows the object 
+//so that whatever changes in arr2 it woul dbe change in arr
