026550168f9148a87fa47f987327ffea8bffbe8f
aditya waykole <adityawaykole494@gmail.com>
2026-06-26 23:08:49 +0530

    how to executed

---
+++ javascript/lecture_16/index.js
@@ -0,0 +1,11 @@
+//execution content 
+//memory allocation 
+//a = unndefined 
+//b = undefined
+//sum1 = undefined
+// addnumber = fn 
+// sum2 = undefined
+
+console.log(a,b );
+let a = 10 ; 
+const b = 20 ; 
